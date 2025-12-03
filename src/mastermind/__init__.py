0e33e19d3213d732ac1811b9ec3b4e745cfbf5c9
Ali Vaezi <alivaezie@gmail.com>
2025-12-03 19:00:06 +0100

    Fix formatting in package initializer

--- src/mastermind/__init__.py
+++ src/mastermind/__init__.py
@@ -1,4 +1,4 @@
-"""Mastermind package initializer."""
+""" Mastermind package initializer."""
 from .engine import Rules, validate_guess, score
 from .game import Game
 from .scoreboard import ScoreEntry, calculate_score, save_score, load_scores, top_scores
@@ -15,3 +15,4 @@ __all__ = [
     "top_scores",
 ]
 __version__ = "0.2.0"  # Updated for Sprint 2
+
