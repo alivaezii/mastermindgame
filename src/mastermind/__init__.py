809e4e74e784612edcd7504b17cb26da43d87477
Ali Vaezi <alivaezie@gmail.com>
2025-12-05 00:44:48 +0100

    Refactor import statements and maintain __all__

--- src/mastermind/__init__.py
+++ src/mastermind/__init__.py
@@ -1,7 +1,14 @@
 """Mastermind package initializer."""
+
 from .engine import Rules, validate_guess, score
 from .game import Game
-from .scoreboard import ScoreEntry, calculate_score, save_score, load_scores, top_scores
+from .scoreboard import (
+    ScoreEntry,
+    calculate_score,
+    save_score,
+    load_scores,
+    top_scores,
+)
 
 __all__ = [
     "Rules",
@@ -14,5 +21,5 @@ __all__ = [
     "load_scores",
     "top_scores",
 ]
+
 __version__ = "0.2.0"  # Updated for Sprint 2
-c
